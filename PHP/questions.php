d70d1ba1da2af1d785c07c75a05653f80bd2df46
Julen Ferrero <julenferre@gmail.com>
2016-10-16 12:45:57 +0200

    Lab4 - Hautazkoak bukatuta (HOSTINGER)

--- PHP/questions.php
+++ PHP/questions.php
@@ -1,18 +1,3 @@
-<?php
-
-	echo "<link rel='stylesheet' type='text/css' href='../CSS/style.css'></link>";
-	
-	//DDBBra konektatu		
-	include "connect.php";
-	
-	// Datuak jaso
-	$query = "SELECT * FROM galderak";
-		
-	$erantzuna = $conn->query($query);
-		
-	$conn->close();
-?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -32,9 +17,9 @@
   <body>
   <div id='page-wrap'>
 	<header class='main' id='h1'>
-      	<span class="right" style="display:inline; float: right;"><a href="signUp.html">Sign Up</a> </span><br/>
-      	<span class="right" style="display:inline; float: right;"><a href="../PHP/signIn.php">Log In</a> </span>
-      	<span class="right" style="display:none; float: right;"><a href="logout.php">LogOut</a> </span>
+      	<span class="right" style="display:inline; float: right;"><a href="../HTML/signUp.html">Sign Up</a> </span><br/>
+      	<span class="right" style="display:inline; float: right;"><a href="signIn.php">Log In</a> </span>
+		<span class="right" style="display:none; float: right;"><a href="logout.php">LogOut</a> </span>
 		<h2>Questions</h2>
     </header>
 	<nav class='main' id='n1' role='navigation'>
@@ -45,13 +30,46 @@
     <section class="main" id="s1">
 	<div id="edukia">
 		<?php
+			//DDBBra konektatu		
+			include "connect.php";
+			
+			// Datuak jaso
+			$query = "SELECT * FROM galderak";
+				
+			$erantzuna = $conn->query($query);
+			
 			if ($erantzuna->num_rows > 0) {
 				while($lerroa = $erantzuna->fetch_assoc()) {
 					echo " -> " . $lerroa['galdera'] . " (Zailtasun maila: " . $lerroa['maila'] . ")<br>";
 				}
 			} else {
 				echo "Ez dago galderarik";
-			} 
+			}
+			//ekintzak taulan datuak sartzen dira			
+			$eposta = NULL;
+			if(session_id() == ''){
+				$eposta = $_SESSION['login_user'];
+			}
+			$kon_id = NULL;
+			if($eposta != NULL){
+				$kon_id_q = "SELECT * FROM konexioak WHERE erab_eposta = '$eposta' ORDER BY ordua DESC LIMIT 1";
+				$kon_id_er = $conn->query($kon_id_q);				
+				while ($kon_id_ler = $kon_id_er->fetch_assoc() ) {
+					$kon_id = $kon_id_ler["id"];
+				} 
+				if($kon_id == NULL){
+					echo "0 results<br>";
+				}
+			}
+			$mota = "Galderak ikusi";
+			$ordua = date('Y/m/d H:i:s');
+			$ip = $_SERVER['REMOTE_ADDR'];
+			$query = "INSERT INTO ekintzak VALUES ('','$kon_id','$eposta', '$mota', '$ordua', '$ip')";
+			if($conn->query($query) === FALSE) {
+				echo "<br/><br/><font color='red'>Ekintzaren datuak ez dira gorde: </font>". $query . "</h2><br>" . $conn->error;
+			}
+			
+			$conn->close();
 		?>
 		<br>
 		<p>Galdera berriak sartzeko, <a href="signIn.php">identifikatu zaitez</a></p>
