788c0408709cc0422a76e78678509d224a24052c
Aimar Ugarte <ugarteaimar@gmail.com>
2016-10-21 12:16:12 +0200

    LAB 5 - ...

--- PHP/signIn.php
+++ PHP/signIn.php
@@ -61,11 +61,12 @@
 								$erantzuna = $conn->query($query);
 								if ($erantzuna->num_rows > 0) {
 									while($lerroa = $erantzuna->fetch_assoc()) {
+										echo $lerroa['id'];
 										$_SESSION['konexio_id'] = $lerroa['id'];
 									}
 								}
 								//Galdera sartzera pasatzen gara
-								header("Location: insertQuestion.php");
+								//header("Location: insertQuestion.php");
 								exit;
 							}
 							else{
