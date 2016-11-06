a8234e314e331dd8cbde60448126492b11ee68f3
Julen Ferrero <julenferre@gmail.com>
2016-11-06 18:15:48 +0100

    Lab7 - Derr bukatuta (Probatu behar da) (HOSTINGER)

--- PHP/egiaztatuPasahitza.php
+++ PHP/egiaztatuPasahitza.php
@@ -4,23 +4,23 @@
 	require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR .'..'.DIRECTORY_SEPARATOR .'..'.DIRECTORY_SEPARATOR .'lib'.DIRECTORY_SEPARATOR . 'NuSOAP' . DIRECTORY_SEPARATOR . 'class.wsdlcache.php');
 	
 	//soap_server motako objektua sortzen dugu
-	$ns=__FILE__; //name of the service
+	$ns=dirname(__FILE__) . DIRECTORY_SEPARATOR .'..' . DIRECTORY_SEPARATOR 'SOAP' . DIRECTORY_SEPARATOR . 'egiaztatuPasahitza.xml'; //name of the service
 	$server = new soap_server;
-	$server->configureWSDL('egiaztatu',$ns);
+	$server->configureWSDL('egiaztatuP',$ns);
 	$server->wsdl->schemaTargetNamespace=$ns;
 	
 	//inplementatu nahi dugun funtzioa erregistratzen dugu
-	$server->register('egiaztatu',
+	$server->register('egiaztatuP',
 		array('pasahitza'=>'xsd:string',),
 		array('erantzuna'=>'xsd:string'),$ns);
 	
 	//funtzioa inplementatzen dugu
-	function egiaztatu($pasahitza){		
+	function egiaztatuP($pasahitza){		
 		//Flag bat
 		$passTxarra = false;
 	
 		//Fitxategia irakurtzen da eta edukia aldagai batean gordetzen da
-		$fitx = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+		$fitx = fopen("../Pasahitzak/toppasswords.txt", "r") or die("Unable to open file!");
 		while ((($lerroa = fgets($fitx)) !== false) || !$passTxarra) {
 			if($lerroa == $pasahitza){
 				$passTxarra = true;
@@ -29,7 +29,7 @@
 		fclose($fitx);
 		
 		if(passTxarra){
-			return "BALIOGABEA";			
+			return "BALIOGABEA";
 		}
 		else{
 			return "BALIOZKOA";
