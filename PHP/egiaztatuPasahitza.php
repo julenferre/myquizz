e7d95d23fe7bc0b358eb75d27e541f7e294357dc
Julen Ferrero <julenferre@gmail.com>
2016-11-07 01:08:37 +0100

    Lab7 - Derr bukatuta (Hostinger) (Aurreko guztiak require-arekin arazoak zeudelako da; espero da oraingoan arazorik ez eukitzea)

--- PHP/egiaztatuPasahitza.php
+++ PHP/egiaztatuPasahitza.php
@@ -1,11 +1,7 @@
 <?php
-	//nusoap.php klasea gehitzen dugu (LOCALHOST)
-	//require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'nusoap.php');
-	//require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'class.wsdlcache.php');
-	
-	//nusoap.php klasea gehitzen dugu (HOSTINGER)
-	require_once('..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'nusoap.php');
-	require_once('..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'class.wsdlcache.php');
+	//nusoap.php klasea gehitzen dugu
+	require_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'nusoap.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'class.wsdlcache.php');
 		
 	//soap_server motako objektua sortzen dugu
 	//$ns="http://localhost:1234/myquizz/PHP/egiaztatuPasahitza.php/egiaztatuP"; //name of the service (localhost)
--- PHP/egiaztatuPasahitzaBezeroa.php
+++ PHP/egiaztatuPasahitzaBezeroa.php
@@ -1,11 +1,7 @@
 <?php	
-	//nusoap.php klasea gehitzen dugu (LOCALHOST)
-	//require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'nusoap.php');
-	//require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'class.wsdlcache.php');
-	
-	//nusoap.php klasea gehitzen dugu (HOSTINGER)
-	require_once('..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'nusoap.php');
-	require_once('..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'class.wsdlcache.php');
+	//nusoap.php klasea gehitzen dugu
+	require_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'nusoap.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'NuSOAP'.DIRECTORY_SEPARATOR.'class.wsdlcache.php');
 	
 	//soapclient motadun objektua sortzen dugu
 	$soapclient = new nusoap_client( 'http://jferrero.esy.es/myquizz-master/PHP/egiaztatuPasahitza.php?wsdl', true); //hostinger
