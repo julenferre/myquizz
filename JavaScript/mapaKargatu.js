2f532224a3385081f383a781b3d487c0522945f2
Aimar Ugarte <ugarteaimar@gmail.com>
2016-11-10 18:04:29 +0100

    LAB 7 - Zerbitzariaren lokalizazioa eginda. Erabiltzailearen lokalizazioan mapa gehitu.

---
+++ JavaScript/mapaKargatu.js
@@ -0,0 +1,40 @@
+//BEZERO MAPA
+function bezeroMapaErakutzi(){
+	maparenDiv = document.getElementById("mapDiv");
+	if(maparenDiv.style.display == "" || maparenDiv.style.display == "none"){
+		maparenDiv.style = "display: block;";
+		myMap();
+	}
+	else if(maparenDiv.style.display == "block"){
+		maparenDiv.style = "display: none;"
+	}
+}
+function myMap() {
+	var mapCanvas = document.getElementById("map");
+  	
+  	var mapOptions = {
+    	center: {lat: 43.26, lng: -2.935},
+    	zoom: 13
+  	}
+  	var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  	var marker = new google.maps.Marker();
+  	
+  	//BEZERO
+  	if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+            	lat: position.coords.latitude,
+              	lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+            marker.setPosition(pos);
+            marker.setMap(map);
+        }, function() {
+        	handleLocationError(true, infoWindow, map.getCenter());
+        });
+    }
+    else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    } 
+}
---
+++ PHP/serverGeolocation.php
@@ -0,0 +1,10 @@
+<?php
+	$ServerIP = $_SERVER['SERVER_ADDR'];
+	//$requestURI = "http://freegeoip.net/json/" . $ServerIP; //ZERBITZARIKO IP-a. HOSTINGERREN BAKARRIK LORTUKO DU
+	$requestURI = "http://freegeoip.net/json/158.227.0.238"; //PROBARAKO IP BAT
+	$json = file_get_contents($requestURI);
+	$array = json_decode($json, true);
+
+	echo $array['country_name'] . ". " . $array["region_name"] . ", " . $array["city"]; 					//Herrialdea eta herria
+	//echo "latitudea: " . round($array["latitude"],3) . ", longitudea: " . round($array["longitude"],3);	//Koordenatuak
+?>
